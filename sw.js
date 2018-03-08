9fdf567cb7fd2241190a390dce02389756fe7fb7
filflora <whirlion@gmail.com>
2018-03-08 03:52:02 +0100

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/blog/app-135aba33f25e3dd700fd.js","6077503aad2f8bb56c12779c45e7d511"],["/blog/commons-6e50dd7dacc5e0d2d891.js","a81f848494e664fc62104c1298b300bf"],["/blog/commons-784705ff4beb256d0ee1.js","b50bbfbb4d55a79d0c25dda6e0e07084"],["/blog/index.html","1a42a177805de1b45cd874cf25d7aeb9"],["/blog/manifest.json","d2d859be92065a0a0b445e1321cc7c3b"],["/blog/offline-plugin-app-shell-fallback/index.html","bb218736f320e19c884751290f9c3ecb"]];
+var precacheConfig = [["/blog/app-135aba33f25e3dd700fd.js","6077503aad2f8bb56c12779c45e7d511"],["/blog/commons-6e50dd7dacc5e0d2d891.js","a81f848494e664fc62104c1298b300bf"],["/blog/commons-784705ff4beb256d0ee1.js","b50bbfbb4d55a79d0c25dda6e0e07084"],["/blog/index.html","a43f858da2e0aefe8c42fbd457e3a616"],["/blog/manifest.json","d2d859be92065a0a0b445e1321cc7c3b"],["/blog/offline-plugin-app-shell-fallback/index.html","c09d45c9739479e929338512fb01bda1"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
